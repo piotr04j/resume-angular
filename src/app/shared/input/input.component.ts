284e1445c0352623ecf189d70a7ee9ddbcd6bec2
piotr04j <darricck04@gmail.com>
2020-07-07 18:01:02 +0200

    added input component

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.sass']
+})
+export class InputComponent implements OnInit {
+  @Input() inputType: string;
+  @Input() inputFormControlName: FormControl;
+  @Input() label: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
