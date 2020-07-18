7eb79cd8b543bac577e76f0495556beff6d67575
piotr04j <darricck04@gmail.com>
2020-07-18 17:24:34 +0200

    added menu component

---
+++ src/app/home/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.sass']
+})
+export class MenuComponent implements OnInit {
+  signedIn: boolean
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.signedIn$.subscribe(value => {
+      this.signedIn = value
+    })
+  }
+
+}
