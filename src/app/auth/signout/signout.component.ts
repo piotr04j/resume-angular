fde5b994c87823ba74c5f088fb3cb918e2883e08
piotr04j <darricck04@gmail.com>
2020-07-18 17:25:03 +0200

    added signout component

---
+++ src/app/auth/signout/signout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-signout',
+  template: '',
+  styles: []
+})
+export class SignoutComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.signOut();
+    this.router.navigateByUrl('/');
+  }
+}
