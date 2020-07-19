ee8bcfa45df9dfce1bcebcb31efe2027a473b3cb
piotr04j <darricck04@gmail.com>
2020-07-19 14:52:10 +0200

    improved sign forms

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../auth.service";
 
 @Component({
@@ -10,8 +10,16 @@ import {AuthService} from "../auth.service";
 export class SigninComponent implements OnInit {
 
   signinForm = new FormGroup({
-    email: new FormControl('', []),
-    password: new FormControl('', [])
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/)
+    ])
   })
 
   constructor(private authService: AuthService) { }
@@ -20,6 +28,10 @@ export class SigninComponent implements OnInit {
   }
 
   onSubmit() {
+    if (this.signinForm.invalid) {
+      return
+    }
+
     const { email, password } = this.signinForm.value;
     this.authService.signIn({email, password}).subscribe((res) => {
       console.log(res)
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../auth.service";
+import {MatchPassword} from "../validators/match-password";
 
 @Component({
   selector: 'app-signup',
@@ -9,16 +10,37 @@ import {AuthService} from "../auth.service";
 })
 export class SignupComponent implements OnInit {
   signupForm = new FormGroup({
-    email: new FormControl('', []),
-    password: new FormControl('', [])
-  })
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/)
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/)
+    ])
+  }, {
+        validators: [this.matchPassword.validate]
+    }
+)
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private matchPassword: MatchPassword) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+    if (this.signupForm.invalid) {
+      return
+    }
+
     const { email, password } = this.signupForm.value;
     this.authService.signUp({email, password}).subscribe((res) => {
       console.log(res)
