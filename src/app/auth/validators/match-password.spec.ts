8e73b1aafee46c443daba8da328b8ec0c75a572f
piotr04j <darricck04@gmail.com>
2020-07-19 15:10:33 +0200

    added custom validator tests

--- src/app/auth/validators/match-password.spec.ts
+++ src/app/auth/validators/match-password.spec.ts
@@ -1,7 +1,32 @@
 import { MatchPassword } from './match-password';
+import {FormControl, FormGroup} from "@angular/forms";
 
 describe('MatchPassword', () => {
+  let form: FormGroup;
+  beforeAll(() => {
+    form = new FormGroup({
+      password: new FormControl(),
+      passwordConfirmation: new FormControl()
+    })
+  })
+
   it('should create an instance', () => {
     expect(new MatchPassword()).toBeTruthy();
+  });
+
+  it('should return null', () => {
+    form.controls['password'].setValue('123qwe');
+    form.controls['passwordConfirmation'].setValue('123qwe');
+    const validator = new MatchPassword();
+
+    expect(validator.validate(form)).toBeNull();
+  });
+
+  it('should return error', () => {
+    form.controls['password'].setValue('qwe123');
+    form.controls['passwordConfirmation'].setValue('123qwe');
+    const validator = new MatchPassword();
+
+    expect(JSON.stringify(validator.validate(form))).toBe(JSON.stringify({passwordsDontMatch: true}));
   });
 });
