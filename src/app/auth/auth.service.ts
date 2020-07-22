6c53f1b01506bc9fd3fc32a8bedd2354940063a5
piotr04j <darricck04@gmail.com>
2020-07-22 18:06:27 +0200

    changed path of import environmentand removed redundant mock in auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environments/environment";
+import {environment} from "src/environments/environment";
 import {User} from "../../models/User";
 import {BehaviorSubject} from "rxjs";
 import {tap} from "rxjs/operators";
