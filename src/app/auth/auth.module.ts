583c8c5bd84c39764f571e79cefc275a037c0a4b
piotr04j <darricck04@gmail.com>
2020-07-28 18:05:11 +0200

    moved shared.module to different dir

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import {AuthRoutingModule} from "./auth-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
-import {SharedModule} from "../shared/shared/shared.module";
+import {SharedModule} from "../shared/shared.module";
 import { SigninComponent } from './signin/signin.component';
 import { SignoutComponent } from './signout/signout.component';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {InputComponent} from "../input/input.component";
+import {InputComponent} from "./input/input.component";
 import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
